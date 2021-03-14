c10bc7dc92a3a81678c807e84a1662bc3496fb66
Ray Lanier <developer@rashundawilliams.com>
2021-03-14 16:51:47 -0400

    Form Validation - added red border to bill amount field when invalid characters are entered

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-document.getElementById("billAmt").addEventListener('keyup', function(){
+const billAmount = document.getElementById("billAmt");
+const tipPercentage = document.getElementById("tipPercentage");
 
+billAmount.value="0.00";
+
+billAmount.addEventListener('keyup', function(){
+    
     calc();
 
 });
 
-document.getElementById("tipPercentage").addEventListener('change', function(){
+tipPercentage.addEventListener('change', function(){
 
     calc();
 
@@ -12,23 +17,17 @@ document.getElementById("tipPercentage").addEventListener('change', function(){
 
 function calc() {
     
-    const billAmount = Number(document.getElementById("billAmt").value);
-    const tipPercentage = Number(document.getElementById("tipPercentage").value);
+    const bill = Number(billAmount.value);
     
-    var tipAmount = billAmount * tipPercentage;
-    var total = billAmount + Number(tipAmount);
+    var tipAmount = bill * Number(tipPercentage.value);
+    var total = bill + Number(tipAmount);
 
-    if (isNaN(billAmount)) {
+    if (isNaN(bill)) {
         document.getElementById("tipAmt").value = "0.00";
         document.getElementById("total").value = "0.00";
     } else {
         document.getElementById("tipAmt").value = Number(tipAmount).toFixed(2);
         document.getElementById("total").value = Number(total).toFixed(2);
     }
-
-
-    
-    
-    
 }
 
